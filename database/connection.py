eb724f639ce96aef1b3ad906667c8b1bc6daa021
Luiz Moreira <62190950+LuizMoreira-py@users.noreply.github.com>
2023-03-06 21:54:51 -0300

    modified:   database/__pycache__/connection.cpython-39.pyc 	modified:   database/connection.py

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine, inspect
 from sqlalchemy_utils import database_exists
-from sqlalchemy import Column, Integer, String, MetaData, Table, Boolean
+from sqlalchemy import Column, Integer, String, MetaData, Table, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class Connection():
     def __init__(self):
-        self.engine = create_engine('postgresql://postgres:postgres@localhost/baseconhecimento')
+        self.engine = create_engine(
+            'postgresql://postgres:postgres@localhost/baseconhecimento')
         self.inspect = inspect(self.engine)
 
         self.metadata = MetaData()
@@ -21,12 +24,23 @@ class Connection():
                     Column('username', String),
                     Column('senha', String),
                     Column('ativo', Boolean, default=True)
-                )
-                
-            self.metadata.create_all(self.engine)   
+                    )
+
+            if not self.inspect.has_table('pessoas'):
+                Table('pessoas', self.metadata,
+                    id = Column(Integer, primary_key=True),
+                    nome = Column(String),
+                    documento = Column(String),
+                    tipo_documento = Column(String),
+                    usuarios_id = Column(Integer, ForeignKey('usuarios.id')),
+                    usuarios = relationship('Usuarios', back_populates='pessoas')
+                    )
+
+
+            self.metadata.create_all(self.engine)
 
             return True
-        
+
         except Exception as e:
             print(e)
             return False
