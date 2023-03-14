2263cbb6e0891400f428a5bb64bc83b081235f29
Luiz Moreira <62190950+LuizMoreira-py@users.noreply.github.com>
2023-03-13 22:23:06 -0300

    Changes to be committed: 	new file:   controllers/__pycache__/controller_permissao.cpython-39.pyc 	new file:   controllers/__pycache__/controller_usuario.cpython-39.pyc 	modified:   controllers/controller_permissao.py 	modified:   database/__pycache__/connection.cpython-39.pyc 	new file:   database/__pycache__/tables.cpython-39.pyc 	modified:   index.py

--- controllers/controller_permissao.py
+++ controllers/controller_permissao.py
@@ -1,9 +1,31 @@
-from ..database.tables import Permissao
-from ..database.connection import Connection
+from database.tables import Permissao
+from database.connection import Connection
+from sqlalchemy.orm import Session
 
-class Controller(Permissao, Connection):
+class ControllerPermissao(Permissao):
     def __init__(self):
         super().__init__()
+        self.connection = Connection()
+
+        self.sessao = Session(bind=self.connection.engine)
     
-    def insert_or_update(self):
-        pass
+    def insert_or_update(self, dados_list):
+        try:
+            for dados_dict in dados_list:
+                if dados_dict.get('id_permissao'):
+                    permissao = self.sessao.query(Permissao).filter_by(id_permissao=dados_dict['id_permissao']).first()
+                    if permissao:
+                        permissao.nivel_permissao = dados_dict['nivel_permissao']
+                        permissao.id_usuario = dados_dict['id_usuario']
+                    
+                else:
+                    permissao_insert = Permissao(nivel_permissao=dados_dict['nivel_permissao'], id_usuario=dados_dict['id_usuario'])
+                    self.sessao.add(permissao_insert)
+
+                self.sessao.commit()
+
+            return {'sucesso': 'permissao inserida ou alterada com sucesso'}, 200
+
+
+        except Exception as e:
+            return {'error': e}, 400
--- index.py
+++ index.py
@@ -1,15 +1,27 @@
 from flask import Flask, request, jsonify
 from controllers.controller_usuario import ControllerUsuario
+from controllers.controller_permissao import ControllerPermissao
 app = Flask(__name__)
 
 @app.route('/baseconhecimento/v1/usuario', methods=['GET', 'POST'])
 def usuario():
     if request.method == 'POST':
         controller = ControllerUsuario()
-
+        
         dados_list = request.get_json()
 
         return jsonify(controller.insert_or_update(dados_list))  
+    else:
+        pass
+
+@app.route('/baseconhecimento/v1/permissao', methods=['GET', 'POST'])
+def permissao():
+    if request.method == 'POST':
+        controller = ControllerPermissao()
+
+        dados_list = request.get_json()
+
+        return jsonify(controller.insert_or_update(dados_list))
     else:
         pass
 
